9cd20a299bd2665b98e8a9af0087f92fd6d48c67
Danh-Huy <ledanhhuy133@gmail.com>
2024-11-21 23:20:43 +0700

    change to sql lite

--- LogisticsWeb/Program.cs
+++ LogisticsWeb/Program.cs
@@ -12,7 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddScoped<IDbInitializer, DbInitializer>();
 builder.Services.AddRazorPages();
